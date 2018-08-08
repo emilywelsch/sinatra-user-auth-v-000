22c1fb3e150f8500886a735b61eded2ad05a0813
Emily Welsch <emwelsch@gmail.com>
2018-08-08 13:07:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,23 +31,23 @@ class ApplicationController < Sinatra::Base
     erb :'sessions/login'
   end
 
-  # post '/sessions' do
-  #   @user = User.find_by(email: params["email"], password: params["password"])
-  #   if @user
-  #     session[:id] = @user.id
-  #     redirect '/users/home'
-  #   end
-  #   redirect 'sessions/login'
-  # end
-
   post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
+    @user = User.find_by(email: params["email"], password: params["password"])
     if @user
-      session[:user_id] = @user.id
+      session[:id] = @user.id
       redirect '/users/home'
     end
-    redirect '/sessions/login'
+    redirect 'sessions/login'
   end
+
+  # post '/sessions' do
+  #   @user = User.find_by(email: params[:email], password: params[:password])
+  #   if @user
+  #     session[:user_id] = @user.id
+  #     redirect '/users/home'
+  #   end
+  #   redirect '/sessions/login'
+  # end
 
   get '/sessions/logout' do
     session.clear
