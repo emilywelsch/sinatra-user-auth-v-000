125500fa0b825f7ee87a89c8a23be58cb30ac523
Emily Welsch <emwelsch@gmail.com>
2018-08-08 13:04:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,62 +15,62 @@ class ApplicationController < Sinatra::Base
     erb :'/registrations/signup'
   end
 
-  # post '/registrations' do
-  #   @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-  #   @user.save
-  #   session[:user_id] = @user.id
-  #   redirect '/users/home'
-  # end
-  #
-  # get '/sessions/login' do
-  #   erb :'sessions/login'
-  # end
-  #
-  # post '/sessions' do
-  #   @user = User.find_by(email: params["email"], password: params["password"])
-  #   if @user
-  #     session[:id] = @user.id
-  #     redirect '/users/home'
-  #   end
-  #   redirect 'sessions/login'
-  # end
-  #
-  # get '/sessions/logout' do
-  #   session.clear
-  #   redirect '/'
-  # end
-  #
-  # get '/users/home' do
-  #   @user = User.find(session[:id])
-  #   erb :'/users/home'
-  # end
-
   post '/registrations' do
-    @user = User.find_by(email: params[:email])
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
     session[:user_id] = @user.id
     redirect '/users/home'
   end
-
+  
   get '/sessions/login' do
     erb :'sessions/login'
   end
-
+  
   post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
+    @user = User.find_by(email: params["email"], password: params["password"])
     if @user
-      session[:user_id] = @user.id
+      session[:id] = @user.id
       redirect '/users/home'
     end
-    redirect '/sessions/login'
+    redirect 'sessions/login'
   end
-
+  
   get '/sessions/logout' do
+    session.clear
     redirect '/'
   end
-
+  
   get '/users/home' do
-    @user = User.find(session[:user_id])
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
+
+  # post '/registrations' do
+  #   @user = User.find_by(email: params[:email])
+  #   session[:user_id] = @user.id
+  #   redirect '/users/home'
+  # end
+  # 
+  # get '/sessions/login' do
+  #   erb :'sessions/login'
+  # end
+  # 
+  # post '/sessions' do
+  #   @user = User.find_by(email: params[:email], password: params[:password])
+  #   if @user
+  #     session[:user_id] = @user.id
+  #     redirect '/users/home'
+  #   end
+  #   redirect '/sessions/login'
+  # end
+  # 
+  # get '/sessions/logout' do
+  #   redirect '/'
+  # end
+  # 
+  # get '/users/home' do
+  #   @user = User.find(session[:user_id])
+  #   erb :'/users/home'
+  # end
 
 end
